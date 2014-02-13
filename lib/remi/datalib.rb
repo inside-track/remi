78d42de9f7099dda9b535c5c4b0b70c24461069d
Sterling Paramore <sterling.paramore@greatvines.com>
2014-02-13 08:31:43 -0800

    Library path initialized with hash

--- lib/remi/datalib.rb
+++ lib/remi/datalib.rb
@@ -2,14 +2,55 @@ module Remi
 
   class Datalib
 
-    def initialize
+    def initialize(args)
+
+      @type = :undefined
+      @options = {}
+
+      unless args.is_a?(Hash)
+        raise "Must pass options hash"
+      end
+
+      if args.has_key?(:directory)
+
+        @type = :directory
+        @options = args[:directory]
+
+        validate_directory_options()
+
+      else
+        raise "Unknown library type #{type}"
+      end
+      
+    end
+
+    def validate_directory_options
+
+      unless @options.has_key?(:dir_name)
+        raise "ERROR: dir_name not defined for directory library"
+      end
+
+      unless @options[:dir_name].is_a?(String)
+        raise "ERROR: dir_name is not a string"
+      end
+
     end
 
     def dataset_exists?
     end
 
     def return_dataset
-    end    
+    end
+
+    def to_s
+
+      if @type == :directory
+        "Datalib: #{@type}, #{@options[:dir_name]}"
+      else
+        "Datalib: Undefined"
+      end
+
+    end
 
   end
 
--- scratch/dev-lib.rb
+++ scratch/dev-lib.rb
@@ -2,10 +2,13 @@
 load "#{File.dirname(__FILE__)}/../lib/remi.rb"
 
 # Re-load my dev library
-load "#{File.dirname(__FILE__)}/../lib/remi/library.rb"
+load "#{File.dirname(__FILE__)}/../lib/remi/datalib.rb"
 
 
+#work = Datalib.new :directory, "/Users/gnilrets/Desktop/work"
+work = Datalib.new :directory => {:dir_name => "/Users/gnilrets/Desktop/work"}
 
+puts work
 
 
 =begin
@@ -17,5 +20,12 @@ datalib <name> <type> <path/options>
 
 work.mydata -- returns a dataset object named mydata in the work library
   # must search through a directory (defined above) and look for dataset with a matching name
+
+
+#-- ways I might accomplish something similar with Ruby
+work = Datalib.new :directory "/Users/gnilrets/Desktop/work"
+
+work = Datalib.new :directory => {:dir_name => "/Users/gnilrets/Desktop/work"}
+
 
 =end
