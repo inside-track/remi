1e46b38dc25ede5170039b2d432ab08d75ec3e03
Sterling Paramore <sterling.paramore@greatvines.com>
2014-03-14 17:37:19 -0700

    trusted header csv works (not pretty)

--- lib/remi/dataview.rb
+++ lib/remi/dataview.rb
@@ -66,7 +66,7 @@ module Remi
 
 
     def table_tpl
-      @tpl = ERB.new <<-EOF #.unindent
+      @tpl = ERB.new <<-EOF.unindent
       <!DOCTYPE html PUBLIC '-//W3C//DTD XHTML 1.0 Strict//EN' 'http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd'>
       <html xmlns='http://www.w3.org/1999/xhtml'>
         <head>
--- test/Dataset/test_csv_read.rb
+++ test/Dataset/test_csv_read.rb
@@ -10,7 +10,7 @@ class Test_csv_read < Test::Unit::TestCase
   def teardown
   end
 
-  def test_csv_read
+  def test_csv_read_custom_headers
     work = @work
 
     csv_file_full_path = File.join(File.dirname(__FILE__),"test_file.csv")
@@ -26,7 +26,8 @@ class Test_csv_read < Test::Unit::TestCase
 #    CSV.open(csv_file_full_path, "r", { :headers => true }) do |rows|
       CSV.open(csv_file_full_path, "r") do |rows|
         rows.each do |row|
-#          puts "row: #{row.inspect}"
+          if $. == 1 then next end # skip header
+
           from_csv[:RAD__Fact_Key] = row[0]
           from_csv[:Distributor__Dim_Key] = row[1]
           from_csv[:RAD__Physical_Cases] = row[7]
@@ -36,37 +37,77 @@ class Test_csv_read < Test::Unit::TestCase
       end
     end
 
+#    Dataview.view work.from_csv
+    
+  end
+
+# YEAH, THIS NEEDS TO BE SIMPLER!!!!
+  
+  def test_csv_read_trust_header
+    work = @work
+    csv_file_full_path = File.join(File.dirname(__FILE__),"test_file.csv")
+
+
+    count_csv_rows = 0
+    Datastep.create work.from_csv do |from_csv|
+
+      puts "-----------"
+      CSV.open(csv_file_full_path, "r", { :headers => true, :return_headers => true }) do |rows|
+        puts rows.inspect
+
+        puts "HEADER_ROW: #{rows.header_row?}"
+        if rows.header_row?
+          rows.readline
+          puts "HEADERS: #{rows.headers}"
+
+          csv_headers = []
+          from_csv.define_variables do
+            rows.headers.each do |header|
+              var header.to_sym, :type => "string"
+              csv_headers << header.to_sym
+            end
+          end
+          puts "csv_headers: #{csv_headers}"
+
+        end
+          
+        rows.each do |row|
+
+          count_csv_rows += 1
+          puts row.inspect
+
+          row.each do |key,value|
+            puts "writing key=#{key}, value=#{value}"
+            from_csv[key.to_sym] = value
+          end
+
+            from_csv.output
+
+        end
+
+
+=begin
+        from_csv.define_variables do
+          var :RAD__Fact_Key, :type => "string"
+          var :Distributor__Dim_Key, :type => "string"
+          var :RAD__Physical_Cases, :type => "number"
+        end
+
+        rows.each do |row|
+          if $. == 1 then next end # skip header
+
+          from_csv[:RAD__Fact_Key] = row[0]
+          from_csv[:Distributor__Dim_Key] = row[1]
+          from_csv[:RAD__Physical_Cases] = row[7]
+
+          from_csv.output
+        end
+=end
+      end
+    end
+
+    assert_equal 10, count_csv_rows, "Expected 10 rows(+1 header), found #{count_csv_rows}"
     Dataview.view work.from_csv
     
   end
 end
-
-=begin
-def serialize_json(in_filename,out_filename,columns,debug=false)
-
-  file = File.open(out_filename,"w")
-  gz = Zlib::GzipWriter.new(file)
-
-
-  sum = 0
-#  CSV.open(in_filename, "r", { :headers => true } ) do |rows|
-  CSV.open(in_filename, "r") do |rows|
-
-
-    rows.each do |row|
-      puts "row: #{row.inspect}" if debug
-      puts "row is class #{row.class}" if debug
-      puts row.to_json if debug
-      gz.puts row.to_json
-      sum = sum + row[columns[:physical_cases]].to_f
-      line_number = $.
-      puts "#{line_number}" if line_number % 50000 == 0
-    end
-
-  end
-  puts "Physical_Cases: sum = #{sum}"
-
-  gz.close
-
-end
-=end
