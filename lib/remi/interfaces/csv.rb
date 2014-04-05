5071e7096077d25b1e6f08245839202fee982f2d
Sterling Paramore <sterling.paramore@greatvines.com>
2014-04-05 14:00:32 -0700

    Largely functional trusted csv reader - think I want to add keep/drop options

--- lib/remi/interfaces/csv.rb
+++ lib/remi/interfaces/csv.rb
@@ -1,12 +1,49 @@
 class CSV
-  def self.datastep(filename,mode,options = Hash.new,&b)
+  def self.datastep(filename,options = Hash.new, header_to_vars: nil, &b)
+    if header_to_vars
+      self.datastep_trust_headers(filename, header_to_vars, options, &b)
+    else
+      self.datastep_explicit_headers(filename, options, &b)
+    end
+  end
+
+
+  def self.datastep_explicit_headers(filename, options, &b)
     # Assume there is always a header, even if we're doing explicit headers
+    # If there is no header, then user will have to specify :skip_n_lines => 0
     dsoptions = { :skip_n_lines => 1}
     dsoptions.merge!(options.delete(:dataset)) if options.has_key?(:dataset)
 
     skip_n_lines = dsoptions[:skip_n_lines]
-    open(filename,mode,options) do |rows|
+    open(filename, "r", options) do |rows|
       rows.each do |row|
+        yield row if $. > skip_n_lines
+      end
+    end
+  end
+
+
+  def self.datastep_trust_headers(filename, header_to_vars, options, &b)
+    dsoptions = { :skip_n_lines => 0}
+    dsoptions.merge!(options.delete(:dataset)) if options.has_key?(:dataset)
+
+    options.merge!({:headers => true, :return_headers => true})
+
+    skip_n_lines = dsoptions[:skip_n_lines]
+    open(filename, "r", options) do |rows|
+
+      if rows.header_row?
+        rows.readline
+
+        Variables.define header_to_vars do |v|
+          rows.headers.each do |header|
+            v.create header.to_sym
+          end
+        end
+      end
+        
+      rows.each do |row|
+        puts "ROW: #{row.inspect}"
         yield row if $. > skip_n_lines
       end
     end
@@ -19,12 +56,13 @@ module Remi
   class Dataset
 
     def read_row_from_csv(row)
+      puts "This is a #{row.class.name}"
       if row.is_a?(Array)
         read_row_from_csv_array(row)
-      elsif row.is_a?(Hash)
+      elsif row.is_a?(CSV::Row)
         read_row_from_csv_hash(row)
       else
-        raise TypeError, "Expecting a csv row Array or Hash"
+        raise TypeError, "Expecting a csv row Array or CSV::Row"
       end
     end
 
@@ -37,12 +75,9 @@ module Remi
 
 
     def read_row_from_csv_hash(row)
-      @vars.each do |var_name,var_obj|
-        if row.has_key?(var_name)
-          self[var_name] = row[var_name]
-        end
+      row.each do |key,value|
+        self[key.to_sym] = value
       end
     end
-
   end
 end
--- test/standard/test_csv_read.rb
+++ test/standard/test_csv_read.rb
@@ -41,12 +41,10 @@ class Test_csv_read < Test::Unit::TestCase
     end
     
     assert_physical_cases
-
   end
 
 
   def test_csv_helper_read_custom_headers
-
     Datastep.create @work.from_csv do |ds|
       Variables.define ds do |v|
         v.create :RAD__Fact_Key, :type => "string", :csv_col => 0
@@ -54,19 +52,16 @@ class Test_csv_read < Test::Unit::TestCase
         v.create :RAD__Physical_Cases, :type => "number", :csv_col => 7
       end
 
-      CSV.datastep(@csv_file_full_path, "r") do |row|
+      CSV.datastep @csv_file_full_path do |row|
         ds.read_row_from_csv(row)
         ds.write_row
       end
     end
     
     assert_physical_cases
-
   end
 
-=begin
   def test_csv_std_read_trust_header
-
     Datastep.create @work.from_csv do |ds|
       CSV.open(@csv_file_full_path, "r", 
                { :headers => true, 
@@ -76,17 +71,14 @@ class Test_csv_read < Test::Unit::TestCase
         if rows.header_row?
           rows.readline
 
-          ds.define_variables do
+          Variables.define ds do |v|
             rows.headers.each do |header|
-              var header.to_sym, :type => "string"
+              v.create header.to_sym
             end
           end
-
         end
           
         rows.each do |row|
-          puts row.inspect
-
           row.each do |key,value|
             ds[key.to_sym] = value
           end
@@ -98,48 +90,15 @@ class Test_csv_read < Test::Unit::TestCase
 
     assert_physical_cases
   end
-
 
   def test_csv_helper_read_trust_header
-
     Datastep.create @work.from_csv do |ds|
-      CSV.open(@csv_file_full_path, "r", 
-               { :headers => true, 
-                 :return_headers => true 
-               }) do |rows|
-
-        puts rows.inspect
-
-        puts "HEADER_ROW: #{rows.header_row?}"
-        if rows.header_row?
-          rows.readline
-          puts "HEADERS: #{rows.headers}"
-
-          csv_headers = []
-          ds.define_variables do
-            rows.headers.each do |header|
-              var header.to_sym, :type => "string"
-              csv_headers << header.to_sym
-            end
-          end
-          puts "csv_headers: #{csv_headers}"
-
-        end
-          
-        rows.each do |row|
-          puts row.inspect
-
-          row.each do |key,value|
-            puts "writing key=#{key}, value=#{value}"
-            ds[key.to_sym] = value
-          end
-
-          ds.write_row
-        end
+      CSV.datastep @csv_file_full_path, header_to_vars: ds do |row|
+        ds.read_row_from_csv(row)
+        ds.write_row
       end
     end
 
     assert_physical_cases
   end
-=end
 end
