b234a2f8ba60a2f1fb612afc6274da54f44c7dc8
Sterling Paramore <sterling.paramore@greatvines.com>
2014-05-16 17:26:16 -0700

    Functional in-memory sort spec

--- spec/sort_spec.rb
+++ spec/sort_spec.rb
@@ -1,67 +1,48 @@
 require "remi_spec"
 
-=begin
-unfinished test that needs to be migrated to a spec when ready
-
-class Test_sort < Test::Unit::TestCase
-
-  def setup
+describe "Sorting datasets" do
+  before do
     @work = Datalib.new :directory => {:dirname => RemiConfig.work_dirname}
 
+    # Create some random data to be sorted
     Datastep.create @work.have do |ds|
-
-      myvar = []
       Variables.define ds do |v|
-        for ivar in 0..4
-          myvar << "myvar_#{('A'..'Z').to_a[ivar]}"
-          v.create myvar[ivar]
-        end
+        v.create :mygrp1
+        v.create :mygrp2, :type => "number"
+        v.create :mydata1, :type => "number"
+        v.create :mydata2, :type => "number"
       end
 
       for irow in 1..100
-
-        ds[myvar[0]] = ('A'..'D').to_a[rand(4)]
-        ds[myvar[1]] = (rand()*100).to_i
-        for ivar in 2..4
-          ds[myvar[ivar]] = rand()
-        end
+        ds[:mygrp1] = ('A'..'D').to_a[rand(4)]
+        ds[:mygrp2] = (rand()*100).to_i
+        ds[:mydata1] = rand()
+        ds[:mydata2] = rand()
 
         ds.write_row
       end
     end
   end
 
-  def teardown
-    # Add a delete data function
-  end
+  describe "in-memory sort" do
+    before do
+      Datastep.sort @work.have, out: @work.sorted, by: [:mygrp1,:mygrp2]
+    end
 
+    it "should be sorted" do
+      Datastep.read @work.sorted do |ds|
+        ds[:mygrp1] <=> ds.prev(:mygrp1)
+        ds[:mygrp1] <=> ds.prev(:mygrp1)
 
-  def test_sort
-    Datastep.sort @work.have, out: @work.sorted, by: [:myvar_A,:myvar_B]
+        next if ds._N_ == 1
+        sorted = ds[:mygrp1] <=> ds.prev(:mygrp1)
+        sorted = (sorted != 0) ? sorted : ds[:mygrp2] <=> ds.prev(:mygrp2)
 
-    last = { :myvar_A => nil, :myvar_B => nil }
-    Datastep.read @work.sorted do |ds|
-      def ds.assign_last(last = {})
-        last[:myvar_A] = self[:myvar_A]
-        last[:myvar_B] = self[:myvar_B]
+        sorted.should be > -1
       end
 
-      if ds._N_ == 1
-        ds.assign_last(last)
-        next
-      end
-
-      sorted = ds[:myvar_A] <=> last[:myvar_A]
-      sorted = (sorted != 0) ? sorted : ds[:myvar_B] <=> last[:myvar_B]
-
-      if sorted < 0
-        assert_equal true, sorted >= 0, "Dataset not sorted | Last: [#{last[:myvar_A]}, #{last[:myvar_B]}]; Current: [#{ds[:myvar_A]}, #{ds[:myvar_B]}]"
-      end
-
-      ds.assign_last(last)
+      Dataview.view @work.sorted
     end
   end
+
 end
-
-
-=end
