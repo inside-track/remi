5f95f30a2889731d7ec02dc92cbdc9b3e70fe2e1
Sterling Paramore <sterling.paramore@insidetrack.com>
2016-01-25 10:48:52 -0800

    Tests for multiple-key aggregations

--- lib/remi/project/jobs/aggregate_job.rb
+++ lib/remi/project/jobs/aggregate_job.rb
@@ -6,9 +6,9 @@ class AggregateJob
 
   define_source :source_data, Remi::DataSource::DataFrame
   define_target :target_data, Remi::DataTarget::DataFrame
+  define_target :multigroup_target_data, Remi::DataTarget::DataFrame
 
   define_transform :main, sources: :source_data, targets: :target_data do
-
     mymin = lambda do |field, df, group_key, indicies|
       values = indicies.map { |idx| df.row[idx][field] }
       "Group #{group_key} has a minimum value of #{values.min}"
@@ -16,5 +16,11 @@ class AggregateJob
 
     target_data.df = source_data.df.aggregate(by: :alpha, func: mymin.curry.(:year)).detach_index
     target_data.df.vectors = Daru::Index.new([:alpha, :year])
+
+    multigroup_target_data.df = source_data.df.aggregate(by: [:alpha,:beta], func: mymin.curry.(:year)).detach_index
+    multigroup_target_data.df.vectors = Daru::Index.new([:alpha_beta, :year])
+
+
+
   end
 end
