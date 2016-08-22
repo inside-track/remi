123c5560e4f962c859baa5b8f8e8963e58d091bc
Sterling Paramore <sterling.paramore@insidetrack.com>
2016-08-22 09:55:20 -0700

    Daru Upgrade - 0.1.4 (#90)

--- lib/remi/monkeys/daru.rb
+++ lib/remi/monkeys/daru.rb
@@ -1,12 +1,4 @@
-# These aren't supporting in Daru 0.1.2.  Can be removed after an upgrade.
-class Daru::Vector
-  def to_df
-    Daru::DataFrame.new({@name => @data}, name: @name, index: @index)
-  end
-end
-
+# Needed to fix issue in Daru 0.1.4.1
 class Daru::DataFrame
-  def to_df
-    self
-  end
+  remove_method :to_hash
 end
