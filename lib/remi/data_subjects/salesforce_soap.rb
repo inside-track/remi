59278e0ad9742099ef30aa4e4649b0ca6bda3564
Sterling Paramore <sterling.paramore@insidetrack.com>
2017-01-20 11:00:54 -0800

    DE-664 - Implement a new Salesforce SOAP Encoder/Loader
    Currently only intended to support the merge command.

---
+++ lib/remi/data_subjects/salesforce_soap.rb
@@ -0,0 +1,98 @@
+require 'soapforce'
+
+module Remi
+  module DataSubject::SalesforceSoap
+    def soapforce_client
+      @soapforce_client ||= begin
+        client = Soapforce::Client.new(host: @credentials[:host], logger: logger)
+        client.authenticate(
+          username: @credentials[:username],
+          password: "#{@credentials[:password]}#{@credentials[:security_token]}"
+        )
+        client
+      end
+    end
+  end
+
+  # Salesforce SOAP encoder
+  class Encoder::SalesforceSoap < Encoder
+    # Converts the dataframe to an array of hashes, which can be used
+    # by the salesforce soap api.
+    #
+    # @param dataframe [Remi::DataFrame] The dataframe to be encoded
+    # @return [Object] The encoded data to be loaded into the target
+    def encode(dataframe)
+      dataframe.to_a[0]
+    end
+  end
+
+  # Salesforce SOAP loader
+  # The Salesforce SOAP loader can be used to merge salesforce objects (for those
+  # objects that support the merge operation).  To do so, each row of the dataframe must
+  # contain a field called `:Id` that references the master record that survives the
+  # merge operation.  It must also contain a `:Merge_Id` field that specifies the
+  # salesforce Id of the record that is to be merged into the master.  Other fields
+  # may also be specified that will be used to update the master record.
+  #
+  # @example
+  #  class MyJob < Remi::Job
+  #    target :merge_contacts do
+  #      encoder Remi::Encoder::SalesforceSoap.new
+  #      loader Remi::Loader::SalesforceSoap.new(
+  #        credentials: { },
+  #        object: :Contact,
+  #        operation: :merge,
+  #        merge_id_field: :Merge_Id
+  #      )
+  #    end
+  #  end
+  #
+  #  job = MyJob.new
+  #  job.merge_contacts.df = Remi::DataFrame::Daru.new({ Id: ['003g000001IX4HcAAL'], Note__c: ['Cheeseburger in Paradise'], Merge_Id: ['003g000001LE7dXAAT']})
+  #  job.merge_contacts.load
+  #
+  class Loader::SalesforceSoap < Loader
+    include Remi::DataSubject::SalesforceSoap
+
+    # @param credentials [Hash] Used to authenticate with salesforce
+    # @option credentials [String] :host Salesforce host (e.g., login.salesforce.com)
+    # @option credentials [String] :username Salesforce username
+    # @option credentials [String] :password Salesforce password
+    # @option credentials [String] :security_token Salesforce security token
+    # @param object [Symbol] Salesforce object to extract
+    # @param operation [Symbol] Salesforce operation to perform (`:merge`) <- Merge is the only operation currently supported
+    # @param merge_id_field [Symbol] For merge operations, this is the name of the field containing the id of the record to be merged (default: :Merge_Id)
+    def initialize(*args, **kargs, &block)
+      super
+      init_salesforce_loader(*args, **kargs, &block)
+    end
+
+    # @param data [Encoder::Salesforce] Data that has been encoded appropriately to be loaded into the target
+    # @return [true] On success
+    def load(data)
+      logger.info "Performing Salesforce Soap #{@operation} on object #{@sfo}"
+      if @operation == :merge
+        # The Soapforce gem only supports one slow-ass merge at a time :(
+        data.each do |row|
+          unless row.include?(@merge_id_field)
+            raise KeyError, "Merge id field not found: #{@merge_id_field}"
+          end
+
+          merge_id = Array(row.delete(@merge_id_field))
+          soapforce_client.merge(@sfo, row, merge_id)
+        end
+      else
+        raise ArgumentError, "Unknown soap operation: #{@operation}"
+      end
+    end
+
+    private
+
+    def init_salesforce_loader(*args, object:, credentials:, operation:, merge_id_field: :Merge_Id, **kargs, &block)
+      @sfo            = object
+      @credentials    = credentials
+      @operation      = operation
+      @merge_id_field = merge_id_field
+    end
+  end
+end
---
+++ spec/data_subjects/salesforce_soap_spec.rb
@@ -0,0 +1,80 @@
+require_relative '../remi_spec'
+require 'remi/data_subjects/salesforce_soap.rb'
+
+
+
+describe Encoder::SalesforceSoap do
+  let(:encoder) { Encoder::SalesforceSoap.new }
+  let(:dataframe) do
+    Daru::DataFrame.new(
+      :Id            => ['003G000001cKYaUIA4', '003G000001cKYbXIA4'],
+      :Student_ID__c => ['FJD385628', nil],
+      :Merge_Id__c   => ['003g000001LE7dXAAT','003g000001IX4HcAAL']
+    )
+  end
+
+  it 'converts the dataframe into an array of hashes' do
+    expected_result = [
+      { :Id => '003G000001cKYaUIA4', :Student_ID__c => 'FJD385628', :Merge_Id__c => '003g000001LE7dXAAT' },
+      { :Id => '003G000001cKYbXIA4', :Student_ID__c => nil,         :Merge_Id__c => '003g000001IX4HcAAL' },
+    ]
+    expect(encoder.encode dataframe).to eq expected_result
+  end
+end
+
+
+describe Loader::SalesforceSoap do
+  let(:loader) { Loader::SalesforceSoap.new(object: :Contact, credentials: {}, operation: :merge) }
+  let(:soapforce_client) { double('soapforce_client') }
+
+  before do
+    allow(loader).to receive(:soapforce_client) { soapforce_client }
+  end
+
+  it 'raises an error if an unknown operation is requested' do
+    data = [
+      { Id: '1234', Custom__c: 'something', Merge_Id: '5678' }
+    ]
+
+    loader = Loader::SalesforceSoap.new(object: :Contact, credentials: {}, operation: :not_defined)
+    expect { loader.load(data) }.to raise_error ArgumentError
+  end
+
+  it 'submits the right merge command' do
+    data = [
+      { Id: '1234', Custom__c: 'something', Merge_Id: '5678' }
+    ]
+
+    expect(soapforce_client).to receive(:merge) do
+      [
+        :Contact,
+        {
+          Id: '1234',
+          Custom__c: 'something'
+        },
+        ['5678']
+      ]
+    end
+
+    loader.load(data)
+  end
+
+  it 'submits a merge command for each row of data' do
+    data = [
+      { Id: '1', Custom__c: 'something', Merge_Id: '10' },
+      { Id: '2', Custom__c: 'something', Merge_Id: '20' }
+    ]
+
+    expect(soapforce_client).to receive(:merge).twice
+    loader.load(data)
+  end
+
+  it 'raises an error if the merge id field is not found' do
+    data = [
+      { Id: '1234', Custom__c: 'something', Alt_Merge_Id: '5678' }
+    ]
+
+    expect { loader.load(data) }.to raise_error KeyError
+  end
+
+end
